84ae49ca86e2a419d623b0e936fceedf9fef6912
Jonatan B. Jåsund <jonatan.jasund@pm.me>
2025-01-11 00:45:33 +0100

    add Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://johnnybangbang.github.io',
   base: 'Astro_Tutorial',
+  integrations: [preact()],
 });
