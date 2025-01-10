81d0e718e3d5383c26495ea42567d0e810d25dc6
Jonatan B. Jåsund <jonatan.jasund@pm.me>
2025-01-10 17:15:28 +0100

    add github stuff to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://johnnybangbang.github.io',
+  base: 'Astro_Tutorial',
+});
